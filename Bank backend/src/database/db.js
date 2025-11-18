f528c19b8bf7f50fdf37c82b4de084179ec64b81
safaa1871 <safaabdullah1871@gmail.com>
2025-11-18 12:28:26 +0300

    Add files via upload

---
+++ Bank backend/src/database/db.js
@@ -0,0 +1,12 @@
+import pkg from "pg";
+const { Pool } = pkg;
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+});
+
+export default pool;
