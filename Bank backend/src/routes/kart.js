c630673874ab3b269321f30303d34c43bae8ddb9
safaa1871 <safaabdullah1871@gmail.com>
2025-11-18 22:21:00 +0300

    Add kart routes for getting and creating items

---
+++ Bank backend/src/routes/kart.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("../controllers/kartController");
+const auth = require("../middlewares/auth");
+
+router.get("/", auth, controller.getAll);
+router.post("/", auth, controller.create);
+
+module.exports = router;
